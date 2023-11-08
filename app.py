2c59500d35caeb53be26e8080750d948b69944bf
KaahinAtNTI <143769422+KaahinAtNTI@users.noreply.github.com>
2023-11-08 06:39:20 +0000

    Major update

--- app.py
+++ app.py
@@ -1,41 +1,103 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+# Initialize the Flask application.
 app = Flask(__name__)
 
+# A global list to store tasks.
 tasks = []
 
-
-# ==================== Routes ====================
+# Define the routes for the application.
 
 
 @app.route("/")
-def index():
+def home():
+    """
+    Serve the index page.
+
+    Returns:
+        Rendered 'index.html' template.
+    """
     return render_template("index.html")
 
 
-# Server Rendered Routes using only Flask and Jinja Templates
+@app.route("/todo", methods=["GET"])
+def todo():
+    """
+    Serve the todo list page.
 
-## Read 
-@app.route("/todo_static", methods=["GET"])
-def todo_page_static():
-    return render_template("todo_static.html", tasks=tasks)
+    Retrieve all tasks and pass them to the 'todo.html' template for display.
+
+    Returns:
+        Rendered 'todo.html' template with all tasks.
+    """
+    return render_template("todo.html", tasks=tasks)
 
 
-## Create
-@app.route("/add_task_static", methods=["POST"])
-def add_task_static():
+@app.route("/add_task", methods=["POST"])
+def add_task():
+    """
+    Add a new task to the list.
+
+    Retrieve task details from the form data and append it to the tasks list.
+
+    Returns:
+        Redirect to the 'todo' route.
+    """
     title = request.form.get("title")
     content = request.form.get("content")
     tasks.append({"title": title, "content": content, "editing": False})
-    return redirect(url_for("todo_page_static"))
+    return redirect(url_for("todo"))
 
 
-## Update
-@app.route("/edit_task_static/<int:task_id>", methods=["POST"])
-def edit_task_static(task_id):
+@app.route("/search_task/", methods=["POST"])
+def search_task():
+    """Searches for a task by ID and redirects to its view page.
+
+    Returns:
+        A redirect to the view_task function with the task_id parameter.
+    """
+    task_id = request.form.get("task_id", type=int)
+    return redirect(url_for("view_task", task_id=task_id))
+
+
+@app.route("/view_task/<int:task_id>", methods=["GET"])
+def view_task(task_id):
+    """
+    Display a single task by its ID.
+
+    Parameters:
+        task_id (int): The ID of the task to display.
+
+    Returns:
+        Rendered 'todo.html' template with the specified task if found, otherwise, an indication that the task was not found.
+    """
+    if 0 <= task_id < len(tasks):
+        # Display only the task that matches the given ID.
+        found_task = [tasks[task_id]]
+        task_found = True
+    else:
+        # If the task isn't found, pass an empty list and a flag to indicate this.
+        found_task = []
+        task_found = False
+
+    return render_template("todo.html", tasks=found_task, task_found=task_found)
+
+
+@app.route("/edit_task/<int:task_id>", methods=["POST"])
+def edit_task(task_id):
+    """Edit a task in the tasks list.
+
+    Args:
+        task_id (int): The ID of the task to be edited.
+
+    Returns:
+        redirect: A redirect to the todo function.
+    """
+    # Extract form data
     title = request.form.get("title")
     content = request.form.get("content")
 
+    # Update task in list
     current_task = tasks[task_id]
     if "editing" in current_task:
         if current_task["editing"] is True:
@@ -51,34 +113,27 @@ def edit_task_static(task_id):
             task["editing"] = False
         current_task["editing"] = True
 
-    return redirect(url_for("todo_page_static"))
+    return redirect(url_for("todo"))
 
 
-## Delete
-@app.route("/delete_task_static/<int:task_id>", methods=["POST"])
-def delete_task_static(task_id):
-    tasks.pop(task_id)
-    return redirect(url_for("todo_page_static"))
+@app.route("/delete_task/<int:task_id>", methods=["POST"])
+def delete_task(task_id):
+    """
+    Delete a task from the list.
+
+    Parameters:
+        task_id (int): The ID of the task to be deleted.
+
+    Returns:
+        Redirect to the 'todo' route.
+    """
+    # Remove the task with the given ID from the list.
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+    return redirect(url_for("todo"))
 
 
-# Hybrid Rendered Routes using htmx, flask and Jinja Templates
-
-@app.route("/todo_dynamic", methods=["GET"])
-def todo_page_dynamic():
-    return render_template("todo_dynamic.html")
-
-
-@app.route("/add_task_dynamic", methods=["POST"])
-def add_task_dynamic():
-    title = request.form.get("title")
-    content = request.form.get("content")
-
-    task = {"title": title, "content": content}
-    tasks.append(task)
-
-    # Return the new task as a card to be added to the UI
-    return render_template("task_card.html", task=task)
-
-
+# The main entry point for the application.
 if __name__ == "__main__":
+    # Start the Flask application with debugging enabled.
     app.run(debug=True)
